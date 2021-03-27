1cd83dc166771050466c7e963a3608f495ba8fb7
kyle(홍민기) <kyle.hong@kakaocorp.com>
2021-03-27 14:50:49 +0900

    15분봉, 일봉 캔들 정보 가져오는 함수 구현

--- main.py
+++ main.py
@@ -8,7 +8,36 @@ access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
 secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
 server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
 
-def getAccountInfo() :
+baseTimeSetting = {
+    "1min": "/minutes/1",
+    "5min": "/minutes/5",
+    "15min": "/minutes/15",
+    "day": "/days",
+    "week": "/weeks",
+    "month": "/months"
+}
+
+def getMinCandle(target_min, market, candle_count):
+    querystring = {
+        "market": market,
+        "count": candle_count
+    }
+    res = requests.request("GET", server_url + "/v1/candles/minutes/" + str(target_min), params=querystring)
+
+    print(res.text)
+
+
+def getDayCandle(market, candle_count):
+    querystring = {
+        "market": market,
+        "count": candle_count
+    }
+    res = requests.request("GET", server_url + "/v1/candles/days", params=querystring)
+
+    print(res.text)
+
+
+def getAccountInfo():
     payload = {
         'access_key': access_key,
         'nonce': str(uuid.uuid4()),
@@ -22,6 +51,9 @@ def getAccountInfo() :
 
     print(res.json())
 
+
 if __name__ == "__main__":
     # 현재 계좌의 상황 체크하기
     getAccountInfo()
+    getMinCandle(15, "KRW-BTC",300)
+    getDayCandle("KRW-BTC", 55)
