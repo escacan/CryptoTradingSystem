40abf8fc924c0c6fa00454557750cfd62f677a35
kyle(홍민기) <kyle.hong@kakaocorp.com>
2021-03-28 00:38:11 +0900

    가격 정보 가져오는 함수 구현

--- binance.py
+++ binance.py
@@ -1,4 +1,6 @@
 import ccxt
+import pprint
+import pandas as pd
 
 def getMarketInfo():
     binance = ccxt.binance()
@@ -7,5 +9,32 @@ def getMarketInfo():
     print(markets.keys())
     print(len(markets))
 
+def getCurrentPrice():
+    binance = ccxt.binance()
+    btc = binance.fetch_ticker("BTC/USDT")
+    pprint.pprint(btc)
+
+def get15MinCandles():
+    binance = ccxt.binance()
+    btc_ohlcv = binance.fetch_ohlcv("BTC/USDT", '15m')
+
+    df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    df.set_index('datetime', inplace=True)
+    print(df)
+
+def get1DayCandles():
+    binance = ccxt.binance()
+    btc_ohlcv = binance.fetch_ohlcv("BTC/USDT", '1d')
+
+    df = pd.DataFrame(btc_ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    df.set_index('datetime', inplace=True)
+    print(df)
+
+
 if __name__ == "__main__":
-    getMarketInfo()
+    # getMarketInfo()
+    # getCurrentPrice()
+    get15MinCandles()
+    get1DayCandles()
