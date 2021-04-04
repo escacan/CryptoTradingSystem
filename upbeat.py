9b78262c8238fbe5cd913cf27d308ee28a3690d9
kyle(홍민기) <kyle.hong@kakaocorp.com>
2021-04-05 03:02:27 +0900

    날짜가 바뀌면 포지션 정리 & 매 분마다 가격 체크

--- upbeat.py
+++ upbeat.py
@@ -9,14 +9,17 @@ import pandas as pd
 import pprint
 import pyupbit
 
-from datetime import date
+import datetime
 
 access_key = os.environ['UPBIT_OPEN_API_ACCESS_KEY']
 secret_key = os.environ['UPBIT_OPEN_API_SECRET_KEY']
 server_url = os.environ['UPBIT_OPEN_API_SERVER_URL']
 upbit = pyupbit.Upbit(access_key, secret_key)
 
-lastDate = 0
+_LAST_CHECKED_DATE = 0
+_LAST_CHECKED_MIN = 0
+_NOTIONAL_BALANCE = 3000000
+_MAXIMUM_RISK = 0.01
 
 def getCoinList():
     return pyupbit.get_tickers(fiat="KRW")
@@ -26,6 +29,15 @@ def getCurrentPrice(ticket):
 
 def getAccountInfo():
     print(upbit.get_balance("KRW"))
+
+def calculateUnitSize(coin, atr):
+    unitSize = 0
+    print("Can order {} : {}".format(coin, unitSize))
+
+    maximumSL = _NOTIONAL_BALANCE * _MAXIMUM_RISK
+    # get Dollar per point
+    # calculate Maximum SL price
+    # get Minimum Order Size
 
 def updateMarketInfo():
     coinList = getCoinList()
@@ -41,13 +53,34 @@ def updateMarketInfo():
             if beforeYesterdayInfo['high'] >= yesterdayInfo['high'] and beforeYesterdayInfo['low'] <= yesterdayInfo['low']:
                 print("Send buylimit order for {} on {} and SL on {}".format(coin, yesterdayInfo['high'], yesterdayInfo['low']))
 
+def clearOrders():
+    coinList = getCoinList()
+    for coin in coinList:
+        # Remove pending orders
+        orderId = upbit.get_order(coin)['uuid']
+        upbit.cancel_order(orderId)
+
+        # Clear every coin positions
+        leftCoin = upbit.get_balance(coin))
+        upbit.sell_market_order(coin, leftCoin))
+
 if __name__ == "__main__":
     getAccountInfo()
-    today = date.today()
+    today = datetime.date.today()
 
-    if lastDate != today:
-        lastDate = today
+    if _LAST_CHECKED_DATE != today:
+        _LAST_CHECKED_DATE = today
+        # Cancle Orders
+        upbit.get_order("KRW-LTC")
+
         updateMarketInfo()
+
+    currentMin = datetime.time.minute()
+    if _LAST_CHECKED_MIN != currentMin:
+        # Check Price
+        _LAST_CHECKED_MIN = currentMin
+        coinList = getCoinList()
+        print(pyupbit.get_current_price(coinList))
 
 
     # 현재 계좌의 상황 체크하기
